aa56457197429fbb3ccb37e762867b16da4ed1bc
Dante De Ruwe <dante.deruwe@student.hogent.be>
2020-10-04 17:01:22 +0200

    Update test to use Identity managers
    these suck, had to make fake ones. Microsoft... smh...

--- DamianTourBackend.Tests/UnitTests/Api/UsersControllerTest.cs
+++ DamianTourBackend.Tests/UnitTests/Api/UsersControllerTest.cs
@@ -1,6 +1,9 @@
 ﻿using DamianTourBackend.Api.Controllers;
+using DamianTourBackend.Core.DTOs;
 using DamianTourBackend.Core.Entities;
 using DamianTourBackend.Core.Interfaces;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
 using NSubstitute;
 using Xunit;
 
@@ -9,22 +12,29 @@ namespace DamianTourBackend.Tests.UnitTests.Api
     public class UsersControllerTest
     {
         private readonly IUserRepository _userRepository;
+        private readonly SignInManager<IdentityUser> _sim;
         private readonly UsersController _sut;
+        private readonly UserManager<IdentityUser> _um;
+        private readonly IConfiguration _config;
 
         public UsersControllerTest()
         {
             _userRepository = Substitute.For<IUserRepository>();
-            _sut = new UsersController(_userRepository);
+            _sim = new FakeSignInManager();
+            _um = new FakeUserManager();
+            _config = Substitute.For<IConfiguration>();
+            _sut = new UsersController(_userRepository, _sim, _um, _config);
         }
 
         [Fact]
         public void Register_NewUser_ShouldRegister()
         {
             // Arrange
-            var user = new User("Doe", "John", "johndoe@email.be", "+32 471 234 567");
+            var registerDTO = new RegisterDTO() { LastName = "Doe", FirstName = "John", Email = "johndoe@email.be", Password = "test", PasswordConfirmation = "test" };
+            var user = new User("Doe", "John", "johndoe@email.be", "+32494567800");
 
             // Act          
-            _sut.Register(user);
+            var result = _sut.Register(registerDTO);
 
             // Assert
             _userRepository.Received().Add(Arg.Any<User>());
