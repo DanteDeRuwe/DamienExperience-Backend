a75a2b329631f6489dde750d9ed926a52f4eab0d
Dante De Ruwe <dante.deruwe@student.hogent.be>
2020-10-04 17:37:36 +0200

    Fix namespace for fake managers

--- DamianTourBackend.Tests/UnitTests/Api/FakeManagers.cs
+++ DamianTourBackend.Tests/UnitTests/Api/FakeManagers.cs
@@ -7,48 +7,51 @@ using NSubstitute;
 using System;
 using System.Threading.Tasks;
 
-public class FakeSignInManager : SignInManager<IdentityUser>
+namespace DamianTourBackend.Tests.UnitTests.Api
 {
-    public FakeSignInManager()
-        : base(
-            new FakeUserManager(),
-            Substitute.For<IHttpContextAccessor>(),
-            Substitute.For<IUserClaimsPrincipalFactory<IdentityUser>>(),
-            Substitute.For<IOptions<IdentityOptions>>(),
-            Substitute.For<ILogger<SignInManager<IdentityUser>>>(),
-            Substitute.For<IAuthenticationSchemeProvider>(),
-            Substitute.For<IUserConfirmation<IdentityUser>>()
-        )
-    { }
-}
-
-public class FakeUserManager : UserManager<IdentityUser>
-{
-    public FakeUserManager()
-        : base(Substitute.For<IUserStore<IdentityUser>>(),
-            Substitute.For<IOptions<IdentityOptions>>(),
-            Substitute.For<IPasswordHasher<IdentityUser>>(),
-            new IUserValidator<IdentityUser>[0],
-            new IPasswordValidator<IdentityUser>[0],
-            Substitute.For<ILookupNormalizer>(),
-            Substitute.For<IdentityErrorDescriber>(),
-            Substitute.For<IServiceProvider>(),
-            Substitute.For<ILogger<UserManager<IdentityUser>>>())
-    { }
-
-    public override Task<IdentityResult> CreateAsync(IdentityUser user, string password)
+    public class FakeSignInManager : SignInManager<IdentityUser>
     {
-        return Task.FromResult(IdentityResult.Success);
+        public FakeSignInManager()
+            : base(
+                new FakeUserManager(),
+                Substitute.For<IHttpContextAccessor>(),
+                Substitute.For<IUserClaimsPrincipalFactory<IdentityUser>>(),
+                Substitute.For<IOptions<IdentityOptions>>(),
+                Substitute.For<ILogger<SignInManager<IdentityUser>>>(),
+                Substitute.For<IAuthenticationSchemeProvider>(),
+                Substitute.For<IUserConfirmation<IdentityUser>>()
+            )
+        { }
     }
 
-    public override Task<IdentityResult> AddToRoleAsync(IdentityUser user, string role)
+    public class FakeUserManager : UserManager<IdentityUser>
     {
-        return Task.FromResult(IdentityResult.Success);
-    }
+        public FakeUserManager()
+            : base(Substitute.For<IUserStore<IdentityUser>>(),
+                Substitute.For<IOptions<IdentityOptions>>(),
+                Substitute.For<IPasswordHasher<IdentityUser>>(),
+                new IUserValidator<IdentityUser>[0],
+                new IPasswordValidator<IdentityUser>[0],
+                Substitute.For<ILookupNormalizer>(),
+                Substitute.For<IdentityErrorDescriber>(),
+                Substitute.For<IServiceProvider>(),
+                Substitute.For<ILogger<UserManager<IdentityUser>>>())
+        { }
 
-    public override Task<string> GenerateEmailConfirmationTokenAsync(IdentityUser user)
-    {
-        return Task.FromResult(Guid.NewGuid().ToString());
-    }
+        public override Task<IdentityResult> CreateAsync(IdentityUser user, string password)
+        {
+            return Task.FromResult(IdentityResult.Success);
+        }
 
+        public override Task<IdentityResult> AddToRoleAsync(IdentityUser user, string role)
+        {
+            return Task.FromResult(IdentityResult.Success);
+        }
+
+        public override Task<string> GenerateEmailConfirmationTokenAsync(IdentityUser user)
+        {
+            return Task.FromResult(Guid.NewGuid().ToString());
+        }
+
+    }
 }
