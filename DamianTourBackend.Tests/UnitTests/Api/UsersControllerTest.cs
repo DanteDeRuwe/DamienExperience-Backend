9035bcd5c3cb8a730fe1f311b764c01b220ab06d
JonasHaenebalcke <47563975+JonasHaenebalcke@users.noreply.github.com>
2020-10-05 18:20:49 +0200

    Added login tests (#6)
    * Added login tests
    
    * Add token regex check in tests
    
    Co-authored-by: Simon Bettens <simon.bettens@student.hogent.be>
    Co-authored-by: Dante De Ruwe <dante.deruwe@student.hogent.be>

--- DamianTourBackend.Tests/UnitTests/Api/UsersControllerTest.cs
+++ DamianTourBackend.Tests/UnitTests/Api/UsersControllerTest.cs
@@ -8,13 +8,16 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using NSubstitute;
+using NSubstitute.ReturnsExtensions;
 using System.Threading.Tasks;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace DamianTourBackend.Tests.UnitTests.Api
 {
     public class UsersControllerTest
     {
+        private readonly ITestOutputHelper _testOutputHelper;
         private readonly IUserRepository _userRepository;
         private readonly SignInManager<IdentityUser> _sim;
         private readonly UsersController _sut;
@@ -23,33 +26,40 @@ namespace DamianTourBackend.Tests.UnitTests.Api
         private readonly IValidator<RegisterDTO> _registerValidator;
         private readonly IValidator<LoginDTO> _loginValidator;
 
-        public UsersControllerTest()
+        public UsersControllerTest(ITestOutputHelper testOutputHelper)
         {
+            _testOutputHelper = testOutputHelper;
             _userRepository = Substitute.For<IUserRepository>();
 
             _sim = new FakeSignInManager();
-            _um = new FakeUserManager();
+            //_um = new FakeUserManager();
+            _um = Substitute.For<FakeUserManager>();
             _config = FakeConfiguration.Get();
             _registerValidator = Substitute.For<IValidator<RegisterDTO>>();
-            _registerValidator = Substitute.For<IValidator<LoginDTO>>();
+            _loginValidator = Substitute.For<IValidator<LoginDTO>>();
 
             _sut = new UsersController(_userRepository, _sim, _um, _config, _loginValidator, _registerValidator);
         }
+        #region Register Tests
 
         [Fact]
-        public async Task Register_GoodUser_ShouldRegisterAsync()
+        public async Task Register_GoodUser_ShouldRegisterAndReturnToken()
         {
             // Arrange
             var registerDTO = DummyData.RegisterDTOFaker.Generate();
+            _um.CreateAsync(Arg.Any<IdentityUser>(), Arg.Any<string>()).Returns(IdentityResult.Success);
             _registerValidator.SetupPass();
 
             // Act          
             var result = await _sut.Register(registerDTO);
 
             // Assert
-            result.Should().BeOfType<CreatedResult>();
+            result.Should().BeOfType<CreatedResult>()
+                .Which.Value.ToString().Should().MatchRegex("^[A-Za-z0-9-_=]+\\.[A-Za-z0-9-_=]+\\.?[A-Za-z0-9-_.+/=]*$");
+
             _userRepository.Received().Add(Arg.Any<User>());
             _userRepository.Received().SaveChanges();
+
         }
 
         [Fact]
@@ -86,5 +96,55 @@ namespace DamianTourBackend.Tests.UnitTests.Api
             _userRepository.DidNotReceive().Add(Arg.Any<User>());
             _userRepository.DidNotReceive().SaveChanges();
         }
+        #endregion
+
+        #region Login Tests
+        [Fact]
+        public async Task Login_ValidationSucces_ShouldReturnToken()
+        {
+            // Arrange
+            var loginDTO = DummyData.LoginDTOFaker.Generate();
+            _loginValidator.SetupPass();
+            _um.FindByNameAsync(loginDTO.Email).Returns(new IdentityUser() { UserName = loginDTO.Email, Email = loginDTO.Email });
+
+            //Act
+            var login = await _sut.Login(loginDTO);
+
+            //Assert
+            login.Should().BeOfType<OkObjectResult>()
+                .Which.Value.ToString().Should().MatchRegex("^[A-Za-z0-9-_=]+\\.[A-Za-z0-9-_=]+\\.?[A-Za-z0-9-_.+/=]*$");
+
+        }
+
+        [Fact]
+        public async Task Login_UserNotFoundOrRegistered_ShouldReturnBadRequest()
+        {
+            // Arrange
+            var loginDTO = DummyData.LoginDTOFaker.Generate();
+            _loginValidator.SetupPass();
+            _um.FindByNameAsync(loginDTO.Email).ReturnsNull();
+
+            //Act
+            var login = await _sut.Login(loginDTO);
+
+            //Assert
+            login.Should().BeOfType<BadRequestResult>();
+
+        }
+
+        [Fact]
+        public async Task Login_ValidationFailed_ShouldNotLoginAndReturnBadRequest()
+        {
+            // Arrange
+            _loginValidator.SetupFail();
+
+            // Act     
+            var loginFail = await _sut.Login(new LoginDTO());
+
+            // Assert
+            loginFail.Should().BeOfType<BadRequestObjectResult>();
+        }
+
+        #endregion
     }
 }
