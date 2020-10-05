e7c375c3899fe895d5d63672e51962697d7e22ea
Dante De Ruwe <dante.deruwe@student.hogent.be>
2020-10-05 12:23:21 +0200

    Add validation tests

---
+++ DamianTourBackend.Tests/UnitTests/Application/Validators/RegisterValidatorTest.cs
@@ -0,0 +1,60 @@
+﻿using DamianTourBackend.Application.Validators;
+using FluentValidation.TestHelper;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace DamianTourBackend.Tests.UnitTests.Application.Validators
+{
+    public class RegisterValidatorTest
+    {
+        private readonly ITestOutputHelper _testOutputHelper;
+        private readonly RegisterValidator _sut;
+
+        public RegisterValidatorTest(ITestOutputHelper testOutputHelper)
+        {
+            _testOutputHelper = testOutputHelper;
+            _sut = new RegisterValidator();
+        }
+
+        [Fact]
+        public void Validate_GoodUser_ShouldPass()
+        {
+            // Arrange
+            var registerDTO = DummyData.RegisterDTOFaker.Generate();
+
+            // Act
+            var result = _sut.TestValidate(registerDTO);
+
+            // Assert          
+            result.ShouldNotHaveAnyValidationErrors();
+        }
+
+        [Theory]
+        [InlineData("plainaddress")]
+        [InlineData("#@%^%#$@#$@#.com")]
+        [InlineData("@example.com")]
+        [InlineData("Joe Smith <email@example.com>")]
+        [InlineData("email.example.com")]
+        [InlineData("email@example@example.com")]
+        [InlineData(".email@example.com")]
+        [InlineData("email.@example.com")]
+        [InlineData("あいうえお@example.com")]
+        [InlineData("email@example.com (Joe Smith)")]
+        [InlineData("email@example")]
+        [InlineData("email@-example.com")]
+        [InlineData("email@example..com")]
+        public void Validate_BadEmail_ShouldFail(string email)
+        {
+            // Arrange
+            var registerDTO = DummyData.RegisterDTOFaker.Generate();
+            registerDTO.Email = email;
+
+            // Act
+            var result = _sut.TestValidate(registerDTO);
+
+            // Assert          
+            result.ShouldHaveValidationErrorFor(x => x.Email);
+        }
+
+    }
+}
--- DamianTourBackend.Tests/UnitTests/DummyData.cs
+++ DamianTourBackend.Tests/UnitTests/DummyData.cs
@@ -1,4 +1,5 @@
 ﻿using Bogus;
+using DamianTourBackend.Application.DTOs;
 using DamianTourBackend.Core.Entities;
 
 namespace DamianTourBackend.Tests.UnitTests
@@ -10,6 +11,13 @@ namespace DamianTourBackend.Tests.UnitTests
             .RuleFor(u => u.LastName, f => f.Person.LastName)
             .RuleFor(u => u.Email, f => f.Person.Email)
             .RuleFor(u => u.PhoneNumber, f => f.Person.Phone);
+
+        public static readonly Faker<RegisterDTO> RegisterDTOFaker = new Faker<RegisterDTO>()
+            .RuleFor(r => r.FirstName, f => f.Person.FirstName)
+            .RuleFor(r => r.LastName, f => f.Person.LastName)
+            .RuleFor(r => r.Email, f => f.Person.Email)
+            .RuleFor(r => r.Password, f => f.Internet.Password(8))
+            .RuleFor(r => r.PasswordConfirmation, (f, r) => r.Password);
 
     }
 }
