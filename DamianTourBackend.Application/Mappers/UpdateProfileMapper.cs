b96a9b70596b710d8440a4d00165e801a188290e
Dante De Ruwe <dante.deruwe@student.hogent.be>
2020-10-09 18:38:11 +0200

    Prevent updating profile with nulls

--- DamianTourBackend.Application/Mappers/UpdateProfileMapper.cs
+++ DamianTourBackend.Application/Mappers/UpdateProfileMapper.cs
@@ -8,15 +8,15 @@ namespace DamianTourBackend.Application.Mappers
     {
         public static void UpdateUser(this UpdateProfileDTO model, ref User user)
         {
-            user.Email = model.Email;
-            user.FirstName = model.FirstName;
-            user.LastName = model.LastName;
+            user.Email = model.Email ?? user.Email;
+            user.FirstName = model.FirstName ?? model.FirstName;
+            user.LastName = model.LastName ?? user.LastName;
         }
 
         public static void UpdateIdentityUser(this UpdateProfileDTO model, ref IdentityUser identityUser)
         {
-            identityUser.Email = model.Email;
-            identityUser.UserName = model.Email;
+            identityUser.Email = model.Email ?? identityUser.Email;
+            identityUser.UserName = model.Email ?? identityUser.UserName;
         }
     }
 }
