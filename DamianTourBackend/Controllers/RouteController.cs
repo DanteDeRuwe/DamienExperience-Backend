5aa7177ba9f8d07fc2e6e1a6defe576c85ee6791
Jonas Haenebalcke <jonashaenebalcke@hotmail.be>
2020-10-29 20:24:04 +0100

    Add documentation to RouteController

--- DamianTourBackend/Controllers/RouteController.cs
+++ DamianTourBackend/Controllers/RouteController.cs
@@ -43,7 +43,11 @@ namespace DamianTourBackend.Api.Controllers
             return Ok(route);
         }
 
-
+        /// <summary>
+        /// Adds a new route
+        /// </summary>
+        /// <param name="routeDTO">Expects a routeDTO</param>
+        /// <returns>Ok request or unauthorized if user is not an admin</returns>
         [HttpPost(nameof(Add))]
         public IActionResult Add(RouteDTO routeDTO) 
         {
@@ -57,6 +61,11 @@ namespace DamianTourBackend.Api.Controllers
             return Ok();
         }
 
+        /// <summary>
+        /// Deletes a route with the given name
+        /// </summary>
+        /// <param name="routeName">Expects a routeName</param>
+        /// <returns>Ok request or unauthorized if user is not an admin</returns>
         [HttpDelete(nameof(Delete))]
         public IActionResult Delete(string routeName)
         {
@@ -70,10 +79,16 @@ namespace DamianTourBackend.Api.Controllers
             return Ok();
         }
 
+        /// <summary>
+        /// Updates a route with using the name of the routeDTO
+        /// </summary>
+        /// <param name="routeDTO">Expects a routeDTO</param>
+        /// <returns>The updated route and ok request or unauthorized </returns>
         [HttpPut(nameof(Update))]
         public IActionResult Update(RouteDTO routeDTO)
         {
             if (!IsAdmin().Result) return Unauthorized();
+
             var route = _routeRepository.GetByName(routeDTO.TourName);
             if (route == null) return BadRequest();
 
@@ -83,7 +98,11 @@ namespace DamianTourBackend.Api.Controllers
             return Ok(route);
         }
 
-        [HttpGet("")]
+        /// <summary>
+        /// Gets all the routes
+        /// </summary>
+        /// <returns>A list of all the routes in an ok request</returns>
+        [HttpGet(nameof(GetAll))]
         public IActionResult GetAll() 
         {
             return Ok(_routeRepository.GetAll());
