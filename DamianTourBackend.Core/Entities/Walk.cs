cae10ea4ad831d2e83d77652e6856d39be89c1ec
Jordy <jordy.vankerkvoorde@student.hogent.be>
2020-11-02 16:07:18 +0100

    Fix add walk

--- DamianTourBackend.Core/Entities/Walk.cs
+++ DamianTourBackend.Core/Entities/Walk.cs
@@ -6,7 +6,7 @@ namespace DamianTourBackend.Core.Entities
     public class Walk
     {
         public Guid Id { get; set; }
-        public Guid UserID { get; set; }
+        //public Guid UserID { get; set; }
         public Guid RouteID { get; set; }
         public DateTime StartTime { get; set; }
         public DateTime EndTime { get; set; }
@@ -14,16 +14,22 @@ namespace DamianTourBackend.Core.Entities
 
         public Walk() { }
 
-        public Walk(DateTime startTime, Route route, User user)
+        public Walk(DateTime startTime, Route route)
         {
+            Id = Guid.NewGuid();
             StartTime = startTime;
             WalkedPath = new Path();
             RouteID = route.Id;
-            UserID = user.Id;
+            //UserID = user.Id;
         }
 
         public void SetCoords(List<double[]> coords) {
             WalkedPath.Coordinates = coords;
         }
+
+        public void AddCoords(List<double[]> coords) {
+            WalkedPath.Coordinates.AddRange(coords);
+        }
+
     }
 }
