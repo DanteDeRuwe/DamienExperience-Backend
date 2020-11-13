801cf84c9113e792cd77be56a01c2e2506badc8f
Jordy <jordy.vankerkvoorde@student.hogent.be>
2020-11-13 22:12:07 +0100

    Clean mailservice

--- DamianTourBackend.Infrastructure/Mailer/MailService.cs
+++ DamianTourBackend.Infrastructure/Mailer/MailService.cs
@@ -28,21 +28,10 @@ namespace DamianTourBackend.Infrastructure.Mailer
         public MailService()
         {
             _client = new HttpClient();
-            //{BaseAddress = new Uri("http://localhost:3000/")};
         }
 
         public void SendCertificate(CertificateDTO dto)
         {
-            /*var stringContent = new JsonContent(dto);
-            var message = new HttpRequestMessage(HttpMethod.Post, new Uri("http://localhost:3000/mailcertificate"));
-            message.Content = stringContent;
-            _client.SendAsync(message);*/
-
-            //https://stackoverflow.com/questions/39134985/how-to-properly-perform-http-post-of-a-json-object-using-c-sharp-httpclient
-
-            /*var JSON = JsonConvert.SerializeObject(dto);
-            var stringContent = new StringContent(JSON, Encoding.UTF8, "application/x-www-form-urlencoded");*/
-
             HttpContent content = new FormUrlEncodedContent(new Dictionary<string, string>
                 {
                     {"id", dto.Id},
@@ -55,36 +44,6 @@ namespace DamianTourBackend.Infrastructure.Mailer
             );
 
             _client.PostAsync("http://localhost:3000/mailcertificate", content);
-
-            /////*
-           /* var httpWebRequest = (HttpWebRequest) WebRequest.Create("http://localhost:3000/mailcertificate");
-            httpWebRequest.ContentType = "application/x-www-form-urlencoded";
-            httpWebRequest.Method = "POST";
-
-            using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
-            {
-                string json = JsonConvert.SerializeObject(dto);
-
-                streamWriter.Write(json);
-            }
-
-            var httpResponse = (HttpWebResponse) httpWebRequest.GetResponse();
-            using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
-            {
-                var result = streamReader.ReadToEnd();
-            }*/
-
         }
-
-        public async Task SendCertificateAsync(CertificateDTO dto) 
-        {
-            throw new NotImplementedException();
-        }
-    }
-
-
-    public class JsonContent : StringContent
-    {
-        public JsonContent(object obj) : base(JsonConvert.SerializeObject(obj), Encoding.UTF8, "application/x-www-form-urlencoded") { }
     }
 }
