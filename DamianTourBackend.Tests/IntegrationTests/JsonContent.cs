cffada7b230a47c00880fdef49c6be29efdd4915
Dante De Ruwe <dante.deruwe@student.hogent.be>
2020-12-13 14:17:17 +0100

    Add basic login integration test

---
+++ DamianTourBackend.Tests/IntegrationTests/JsonContent.cs
@@ -0,0 +1,11 @@
+﻿using System.Net.Http;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace DamianTourBackend.Tests.IntegrationTests
+{
+    public class JsonContent : StringContent
+    {
+        public JsonContent(object value) : base(JsonConvert.SerializeObject(value), Encoding.UTF8, "application/json") { }
+    }
+}
---
+++ DamianTourBackend.Tests/IntegrationTests/LoginTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading.Tasks;
+using DamianTourBackend.Api;
+using DamianTourBackend.Application.Login;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace DamianTourBackend.Tests.IntegrationTests
+{
+    public class LoginTest : IDisposable
+    {
+        private WebApplicationFactory<Startup> _factory;
+
+        public LoginTest()
+        {
+            _factory = new WebApplicationFactory<Startup>();
+        }
+        
+        [Fact]
+        public async Task Login()
+        {
+            // Arrange
+            var client = _factory.CreateClient();
+            var loginDTO = new LoginDTO() {Email = "string@string.string", Password = "stringstring"};
+            var loginContent = new JsonContent(loginDTO);
+            
+            // Act
+            var response = await client.PostAsync("/api/login", loginContent);
+            var content = await response.Content.ReadAsStringAsync();
+    
+            // Assert
+            content.Should().MatchRegex("^[A-Za-z0-9-_=]+\\.[A-Za-z0-9-_=]+\\.?[A-Za-z0-9-_.+/=]*$");
+        }
+
+        public void Dispose() => _factory?.Dispose();
+    }
+}
